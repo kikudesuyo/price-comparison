36ee11a9572c9cfd0a74940647a994ccf40e3831
kikudesuyo <kikuchi.hiromu.609@s.kyushu-u.ac.jp>
2022-12-10 21:33:32 +0900

    change the way to display

--- src/draw_graph.py
+++ src/draw_graph.py
@@ -1,18 +1,11 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from util import read_csv
 
-with open("../data/hoge-date.txt") as f:
-  lines = f.readlines()
-  s_lines = [line.strip() for line in lines]
-  today = s_lines[-1]
-  
-with open("../data/hoge-price.txt") as file:
-  lines = file.readlines()
-  s_lines = [line.strip() for line in lines]
-  today_price = s_lines[-1]    
-
-date_x_axis = ["12/01", "12/02", "12/03", today]
-price_y_axis = [20, 50, 40, int(today_price)]
-plt.plot(date_x_axis, price_y_axis)
+dubble_list = read_csv("/data/mouse.csv")
+today = dubble_list[0]
+today_price = dubble_list[1]
+today_price = list(map(int, today_price))
+plt.plot(today, today_price)
 plt.title("Price Transition")
 plt.show()
--- src/util.py
+++ src/util.py
@@ -1,11 +1,12 @@
 from pathlib import Path
 from urllib import request
+import re
 
 def generate_path(path):
     """絶対パスを生成する
 
     Args:
-      path (str): locator/からの絶対パス
+      path (str): price-comparison/からの絶対パス
 
     Return:
         str: 引数pathへの絶対パス
@@ -13,14 +14,22 @@ def generate_path(path):
     return str(Path(__file__).parents[1]) + path
 
 
-def read_csv(name):
-  path = generate_path("/data/hoge.csv")
-  with open(path, 'r') as data:
-    dates = []
-    prices = []
-    for line in data:
-      [date, price] = line.split(',')
-      dates.append(date)
-      prices.append(price)       
-  return [dates, prices]
-
+def read_csv(product_file):
+    """日付と価格を出力
+    
+    Args:
+        procuct_file(str): 対象のファイル
+    
+    Return:
+        dates(str): 今日の日付
+        price(str): 今日の価格
+    """
+    path = generate_path("/" + product_file)
+    with open(path, 'r') as data:
+      dates = []
+      prices = []
+      for line in data:
+        [date, price] = line.split(',')
+        dates.append(date)
+        prices.append(price)  
+    return [dates, prices]
